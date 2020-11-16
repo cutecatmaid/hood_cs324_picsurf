92b317dbc3c1df81a2370fe18a56abfbd32b22fc
jpoe15 <71941193+jpoe15@users.noreply.github.com>
2020-11-15 20:00:28 -0500

    Update search.php

--- search.php
+++ search.php
@@ -59,8 +59,9 @@
 	</div>
 	</center>
 
-		<div class="gallery"><script src="./app.js"></script></div>
+		<div class="gallery"></div>
 		<div class="next">Next Page</div>
+		<script src="./app.js"></script>
 </div>
 <div id="footer">
 </div>
